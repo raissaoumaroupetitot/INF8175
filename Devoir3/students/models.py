321c14bdff5edc65de054d0ec6da48c50edd03ae
Raissa Oumarou Petitot <raissa.oumarou-petitot@polymtl.ca>
2026-04-14 14:35:44 -0400

    devoir3: part 1 done, part 3:90% done

--- Devoir3/students/models.py
+++ Devoir3/students/models.py
@@ -29,6 +29,7 @@ class PerceptronModel(object):
         Returns: a node containing a single number (the score)
         """
         "*** TODO: COMPLETE HERE FOR QUESTION 1 ***"
+        return nn.DotProduct(x, self.w)
 
     def get_prediction(self, x: nn.Constant) -> int:
         """
@@ -37,12 +38,22 @@ class PerceptronModel(object):
         Returns: 1 or -1
         """
         "*** TODO: COMPLETE HERE FOR QUESTION 1 ***"
+        return 1 if nn.as_scalar(self.run(x)) >= 0 else -1
 
     def train(self, dataset: PerceptronDataset) -> None:
         """
         Train the perceptron until convergence.
         """
         "*** TODO: COMPLETE HERE FOR QUESTION 1 ***"
+        while True:
+            made_update = False
+            for x, y in dataset.iterate_once(1):
+                label = nn.as_scalar(y)
+                if self.get_prediction(x) != label:
+                    self.w.update(x, label)
+                    made_update = True
+            if not made_update:
+                break
 
 
 class RegressionModel(object):
@@ -104,6 +115,14 @@ class DigitClassificationModel(object):
     def __init__(self) -> None:
         # Initialize your model parameters here
         "*** TODO: COMPLETE HERE FOR QUESTION 3 ***"
+        hidden_size1 = 200
+        hidden_size2 = 100
+        self.w1 = nn.Parameter(784, hidden_size1)
+        self.b1 = nn.Parameter(1, hidden_size1)
+        self.w2 = nn.Parameter(hidden_size1, hidden_size2)
+        self.b2 = nn.Parameter(1, hidden_size2)
+        self.w3 = nn.Parameter(hidden_size2, 10)
+        self.b3 = nn.Parameter(1, 10)
 
     def run(self, x: nn.Constant) -> nn.Node:
         """
@@ -120,6 +139,9 @@ class DigitClassificationModel(object):
                 (also called logits)
         """
         "*** TODO: COMPLETE HERE FOR QUESTION 3 ***"
+        hidden1 = nn.ReLU(nn.AddBias(nn.Linear(x, self.w1), self.b1))
+        hidden2 = nn.ReLU(nn.AddBias(nn.Linear(hidden1, self.w2), self.b2))
+        return nn.AddBias(nn.Linear(hidden2, self.w3), self.b3)
 
     def get_loss(self, x: nn.Constant, y: nn.Constant) -> nn.Node:
         """
@@ -135,9 +157,28 @@ class DigitClassificationModel(object):
         Returns: a loss node
         """
         "*** TODO: COMPLETE HERE FOR QUESTION 3 ***"
+        return nn.SoftmaxLoss(self.run(x), y)
 
     def train(self, dataset: DigitClassificationDataset) -> None:
         """
         Trains the model.
         """
         "*** TODO: COMPLETE HERE FOR QUESTION 3 ***"
+        learning_rate = -0.1
+        batch_size = 100
+        params = [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3]
+        steps = 0
+        max_steps = 4000
+
+        for x, y in dataset.iterate_forever(batch_size):
+            steps += 1
+            loss = self.get_loss(x, y)
+            grads = nn.gradients(loss, params)
+            for param, grad in zip(params, grads):
+                param.update(grad, learning_rate)
+
+            if steps % 100 == 0 and dataset.get_validation_accuracy() >= 0.97:
+                break
+
+            if steps >= max_steps:
+                break
